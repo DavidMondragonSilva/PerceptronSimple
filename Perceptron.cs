da4aac0b7b5b7c0b1f8046fe869ed0c4af15b870
DavidMondragonSilva <104805416+DavidMondragonSilva@users.noreply.github.com>
2023-09-05 13:53:16 -0600

    Add files via upload

---
+++ Perceptron.cs
@@ -0,0 +1,129 @@
+﻿using MathNet.Numerics;
+using MathNet.Numerics.LinearAlgebra;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Windows.Forms.DataVisualization.Charting;
+
+
+namespace PerceptronSimple
+{
+    public partial class Perceptron : Form
+    {
+        private List<double> valorX = new List<double>();
+        private List<double> valorY = new List<double>();
+
+        private double w0 = 1;
+        private double w1 = 0;
+        private double w2 = 0;
+        private double b = 0;
+
+        public Perceptron()
+        {
+            InitializeComponent();
+
+            //Maximos, minimos y linea cruzada
+            chart2.ChartAreas[0].AxisX.Minimum = -10;
+            chart2.ChartAreas[0].AxisY.Minimum = -10;
+            chart2.ChartAreas[0].AxisX.Maximum = 10;
+            chart2.ChartAreas[0].AxisY.Maximum = 10;
+            chart2.ChartAreas[0].AxisX.Crossing = 0;
+            chart2.ChartAreas[0].AxisY.Crossing = 0;
+
+            //Configuraciones extras de diseño
+            chart2.ChartAreas[0].AxisX.ArrowStyle = AxisArrowStyle.Triangle;
+            chart2.ChartAreas[0].AxisY.ArrowStyle = AxisArrowStyle.Triangle;
+            chart2.ChartAreas[0].AxisX.LineWidth= 2;
+            chart2.ChartAreas[0].AxisY.LineWidth= 2;
+            chart2.ChartAreas[0].AxisX.LabelAutoFitMinFontSize = 14;
+            chart2.ChartAreas[0].AxisY.LabelAutoFitMinFontSize = 14;
+
+            //Colores
+            chart2.Series[0].Color = Color.Green;
+            chart2.Series[1].Color = Color.Red;
+            chart2.Series[2].Color = Color.Black;
+            chart2.Series[3].Color = Color.Blue;
+        }
+
+        private void chart2_MouseClick(object sender, MouseEventArgs e)
+        {
+            valorX.Add(chart2.ChartAreas[0].AxisX.PixelPositionToValue(e.X));
+            valorY.Add(chart2.ChartAreas[0].AxisY.PixelPositionToValue(e.Y));
+
+            chart2.Series[3].Points.AddXY(valorX.Last(), valorY.Last());
+        }
+
+        private void btnIngresar_Click(object sender, EventArgs e)
+        {
+            if(String.IsNullOrEmpty(tbxPeso1.Text) || 
+                String.IsNullOrEmpty(tbxPeso2.Text) || 
+                String.IsNullOrEmpty(tbxBias.Text))
+            {
+                MessageBox.Show("Llene el formulario correctamente");
+                return;
+            }
+            if (chart2.Series[0].Points.Count < 1 &&
+                chart2.Series[1].Points.Count < 1 && 
+                chart2.Series[3].Points.Count < 1)
+            {
+                MessageBox.Show("Ingrese almenos 1 punto en el plano");
+                return;
+            }
+
+            chart2.Series[0].Points.Clear();
+            chart2.Series[1].Points.Clear();
+            chart2.Series[2].Points.Clear();
+            chart2.Series[3].Points.Clear();
+
+            w1 = double.Parse(tbxPeso1.Text);
+            w2 = double.Parse(tbxPeso2.Text);
+            b = double.Parse(tbxBias.Text);
+
+            LimpiarTextos();
+
+
+            for (int i = 0; i < valorX.Count; i++)
+            {
+                if (FuncionDeActivacion(valorX[i], valorY[i]))
+                {
+                    chart2.Series[0].Points.AddXY(valorX[i], valorY[i]);
+                }
+                else
+                {
+                    chart2.Series[1].Points.AddXY(valorX[i], valorY[i]);
+                }
+            }
+
+            Vector<double> valoresX = Vector<double>.Build.Dense(Generate.LinearSpaced(100,-10,10));
+
+            Vector<double> valoresY = (-w1 * valoresX - b) / w2;
+
+
+            chart2.Series[2].Points.Clear();
+            chart2.Series[2].Points.DataBindXY(valoresX.ToList(), valoresY.ToList());
+        }
+
+
+        #region Validaciones
+
+        private bool FuncionDeActivacion(double x, double y)
+        {
+            return (w0 * b) + (x * w1) + (y * w2) >= 0.5;
+        }
+
+        private void LimpiarTextos()
+        {
+            tbxPeso1.Clear();
+            tbxPeso2.Clear();
+            tbxBias.Clear();
+        }
+
+        #endregion
+    }
+}
